c0f493ac57026fdd7a6991af466563a6a947eb65
russellella <116303658+russellella@users.noreply.github.com>
2023-02-08 11:31:35 -0800

    Complete code
    -Finished code to make functioning stock picker
    -Added if statement to add most profitable days to final array

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,46 +1,19 @@
-# sell_price, buy_price, profit -> create an array
-# Master array?
-# For each day(index) - need to create a new array with 3 values
-
 stocks = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(array)
-  profit_array = []
+  profit_array = [0,0,0]
   array.each_with_index do |value, index|
-    #Day 0 - iterate through for each following index - need to figure out this
-
-    profit = (array[index+1]) - value #Some issue with array[index+1]
-    # while index+1 < array.length ?
-    #Push to mini array
-    arr = Array.new.push(index, index+1, profit) #This works?
-    #Push mini-array to master array
-    profit_array.push(arr)
-    
-# Iterate through days and repeat iterating for each following index
-  index += 1 #move to next index?
-    # Repeat pushing to arrays
-
-# Take master array - compare array.last of all mini-arrays to find biggest
-# Return mini array that has the biggest array.last
+    final_index = index+1
+    while (final_index) < array.length do
+      profit = array[final_index] - value
+      if profit > profit_array[2]
+        profit_array.clear
+        profit_array.push(index, final_index, profit)
+      end
+      final_index = final_index+1
+    end
   end
-end
-
-# 2-6-2023 Attempt
-stocks = [17,3,6,9,15,8,6,1,10]
-
-def stock_picker(array)
-  profit_array = []
-  array.each_with_index do |value, index|
-    profit = ((array[index+1]) - value)
-    arr = Array.new.push(index, index+1, profit)
-    profit_array.push(arr)
-  end
-  p profit_array
+  profit_array
 end
 
 p stock_picker(stocks)
-  
-
-
-# Fetch?
-result = arr.fetch(1) - arr.fetch(0)
